98f22a6539f07f2ef750ef85901dfaae84fd2078
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-05-24 18:59:51 +0200

    Fix

--- src/ProgramDuon.Training.Infrastructure/Fakers/UserFaker.cs
+++ src/ProgramDuon.Training.Infrastructure/Fakers/UserFaker.cs
@@ -9,6 +9,7 @@ public class UserFaker : Faker<User>
     {
         StrictMode(true);
         RuleFor(p => p.Id, f => f.IndexFaker);
+        RuleFor(p => p.Username, f => f.Person.UserName);
         RuleFor(p => p.FirstName, f => f.Person.FirstName);
         RuleFor(p => p.LastName, f => f.Person.LastName);
         RuleFor(p => p.Email, (f, user) => $"{user.FirstName}.{user.LastName}@programduo.se"); // {firstname}.{lastname}@programduo.se
